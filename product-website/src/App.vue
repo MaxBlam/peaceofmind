c57773dacbdb89bbe75ba765c108557e9897387a
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2021-07-12 14:47:15 +0200

    Change to Bootstrap
    Migration from Vuetify to Bootstrap

--- product-website/src/App.vue
+++ product-website/src/App.vue
@@ -1,19 +1,11 @@
 <template>
-  <v-app>
-    <router-link to="/about">TEst</router-link>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
 
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
-</script>
