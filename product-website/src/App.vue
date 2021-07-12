144d211cce49db1656d629cf1f5831383b3149f9
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2021-07-12 17:01:08 +0200

    Navbar Update

--- product-website/src/App.vue
+++ product-website/src/App.vue
@@ -1,11 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-expand-lg bg-light container-fluid">
+      <ul
+        class="collapse navbar-collapse navbar-nav mb-2 mb-lg-0 d-flex flex-row justify-content-center"
+      >
+        <li class="nav-item">
+          <a class="navbar-brand" href="#">
+            <img src="./assets/logo.png" alt="" width="30" />
+          </a>
+        </li>
+        <li class="nav-item ms-auto">
+          <router-link to="/" class="nav-link text-dark">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link text-dark"
+            >About</router-link
+          >
+        </li>
+        <li class="nav-item me-auto">
+          <a
+            class="nav-link text-dark"
+            href="https://github.com/MaxBlam/pieceofmind"
+            >Github</a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#">Contact</a>
+        </li>
+      </ul>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarToggleExternalContent"
+        aria-controls="navbarToggleExternalContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+    </nav>
+    <router-view />
   </div>
 </template>
-
-
