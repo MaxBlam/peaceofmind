c5ffef5e75cfb28018dc468cf479d4139af6ad0b
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2021-07-19 19:38:52 +0200

    Parallax set up

--- product-website/src/main.js
+++ product-website/src/main.js
@@ -1,13 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import AOS from "aos"
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import AOS from 'aos';
+import Rellax from 'rellax';
 
 AOS.init();
-
-Vue.config.productionTip = false
+new Rellax('.rellax', {
+  speed: -2,
+  center: false,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false,
+});
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
