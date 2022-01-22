52f9f9ce211ee4ccc55f2ab6051fc8742b0b385c
Moritz Miedler <miedler.m03@htlwienwest.at>
2022-01-22 15:20:33 +0100

    Test Login Setup

--- server/TESTING/src/components/HelloWorld.vue
+++ server/TESTING/src/components/HelloWorld.vue
@@ -1,58 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div>
+      <button class="btn bg-identity text-light p-2" @click="login()">Sign In with Google</button>
+      <button class="btn bg-identity text-light p-2" @click="register()">Register</button>
+      <span>logged in: {{ loggedIn }}</span>
+      <button class="btn bg-identity text-light p-2" @click="logout()">Logout</button>
+    </div>
+
+    <div></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      loggedIn: false,
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const googleUser = await this.$gAuth.signIn();
+        const goaRes = await googleUser.grantOfflineAccess({
+          scope: 'https://www.googleapis.com/auth/drive.file',
+        });
+        const res = await axios({
+          url: 'http://localhost:3000/login',
+          method: 'POST',
+          'Content-Type': 'application/json',
+          data: {
+            code: goaRes.code,
+          },
+        });
+        if (res.data.code !== 401) {
+          localStorage.setItem('userHash', res.data.data);
+          this.loggedIn = true;
+        } else alert(res.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async register() {
+      const googleUser = await this.$gAuth.signIn();
+      const goaRes = await googleUser.grantOfflineAccess({
+        scope: 'https://www.googleapis.com/auth/drive.file',
+      });
+      const res = await axios({
+        url: 'http://localhost:3000/register',
+        method: 'POST',
+        'Content-Type': 'application/json',
+        data: {
+          code: goaRes.code,
+        },
+      });
+      if (res.data.code === 401) alert(res.data.data);
+    },
+    async logout() {
+      await axios({
+        method: 'GET',
+        url: 'http://localhost:3000/logout',
+      });
+      localStorage.clear();
+      this.loggedIn = false;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
--- server/TESTING/src/main.js
+++ server/TESTING/src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import GAuth from 'vue-google-oauth2';
+const gauthOption = {
+  clientId: '510930460352-helprk2rbp57j1p85tsv275pgopj2ldm.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account',
+};
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
+
+Vue.use(GAuth, gauthOption);
