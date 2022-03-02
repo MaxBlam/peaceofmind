b0ef4eae6be4f0724a45b869961a29935d061845
Moritz Miedler <miedler.m03@htlwienwest.at>
2022-03-02 08:57:15 +0100

    Cypress Google LOGIN

--- client/tests/e2e/specs/test.js
+++ client/tests/e2e/specs/test.js
@@ -1,7 +1,12 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('FOLDER_CREATE_NONAME_NOGRADE_NOTEACHER', () => {
-  it('Creates a new Folder without a name, grade and teacher', () => {
-    expect(true).to.equal(true);
+describe('Google', function () {
+  beforeEach(function () {
+    cy.task('db:seed');
+    cy.loginByGoogleApi();
+  });
+
+  it('shows onboarding', function () {
+    cy.contains('Get Started').should('be.visible');
   });
 });
